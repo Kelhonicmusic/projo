6c618cd68b8054ce3b163d63efd86a8842604141
Kelhonicmusic <89862866+Kelhonicmusic@users.noreply.github.com>
2024-11-11 21:56:02 +0300

    Update settings.py

--- english_learning/english_learning/settings.py
+++ english_learning/english_learning/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-k7rjtg_j--vc4u25stm3qaejl)mi^pu1j6&t6hx+7ce4+*&ojg
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['projo-3.onrender.com']
+ALLOWED_HOSTS = ['projo-f2gm.onrender.com']
 
 
 
